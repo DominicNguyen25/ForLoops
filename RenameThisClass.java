45970e9fdfdf38360ded7e03188629d3f26e5777
Dominic Nguyen <dominic.nguyen@mrjaffesclass.com>
2022-09-15 14:21:50 -0700

    9/15/22 stuff

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,6 +8,10 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
+        
         System.out.println("Hello World!");
     }
 }

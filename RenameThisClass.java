48c1814cbddb75b804942c2e362f014468382cc4
Dominic Nguyen <dominic.nguyen@mrjaffesclass.com>
2022-10-04 14:20:54 -0700

    10/4

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -7,10 +7,12 @@
  */
 public class RenameThisClass
 {
-    public static void main(String[] args) {
-    String string1 = "I am a string";
-    String string2 = string1;
-    String string3 = "I am also a string";
-    boolean stringsEqual = string1.equals(string3);
+    public void run() {
+        int i = 10;
+        int k = 15;
+        int a = 5;
+        int b = 20;
+        
+    
     }
 }

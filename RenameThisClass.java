3177ca8630e4ba3b8642fd498b41d9f77dae1bec
Dominic Nguyen <dominic.nguyen@mrjaffesclass.com>
2022-10-05 14:22:59 -0700

    10/5 WhileLoops

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -12,7 +12,27 @@ public class RenameThisClass
         int k = 15;
         int a = 5;
         int b = 20;
-        
-    
+
+        while (i < 15) {
+            System.out.println("i is " + i + " but is less than 10 ");
+            i += 1;
+        }
+        while (k > 5) {
+            System.out.println("k is " + k + " but is greater than 5");
+            k -= 1;
+        }
+        while (a < 10 && b <= 20 ) {
+            System.out.println("a is " + a + " but is less than 10");
+            System.out.println("b is " + b + " but less than or equal to 20");
+            a += 1;
+            b += 1;
+
+        }
+    }
+
+    public static void main(String[] args)
+    {        
+        RenameThisClass hw = new RenameThisClass();
+        hw.run();
     }
 }
